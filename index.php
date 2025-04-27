76ea2fff81315b0c08c6a561de6bf8d1cf877261
Eduardo Di Lauro <elauro@magnasistemas.com.br>
2025-04-27 17:03:27 -0300

    feat: versão 1 linktree

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+// index.php
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teapot Wear</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Google Tag Manager -->
+    <script>
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-XXXXXXX');
+    </script>
+    <!-- End Google Tag Manager -->
+</head>
+<body>
+
+    <!-- Google Tag Manager (nos primeiros pixels do body) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager -->
+
+    <div class="container text-center">
+        <div class="logo">
+            <img src="https://static.wixstatic.com/media/23b8aa_81b82cc0cf5d49f2ad7d394bd4df31a7~mv2.png" alt="Teapot Wear">
+        </div>
+
+        <div class="links">
+            <a href="https://www.teapotwear.com" class="link-button" target="_blank">Loja Oficial</a>
+            <a href="https://instagram.com/teapot.wear" class="link-button" target="_blank">Instagram</a>
+            <a href="https://www.tiktok.com/@teapot.wear" class="link-button" target="_blank">TikTok</a>
+            <a href="https://www.youtube.com/@teapotwear" class="link-button" target="_blank">YouTube</a>
+        </div>
+    </div>
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Custom JavaScript -->
+    <script src="js/script.js"></script>
+</body>
+</html>
